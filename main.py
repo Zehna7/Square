3f0aac8dae9346658eef5310888f78eb4c2e0570
Zehna7 <zehna.omar2012@gmail.com>
2024-10-17 00:49:48 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+turtle.Screen().setup(300,400)
+square = turtle.Turtle()
+for i in range(4):
+    square.forward(100)  
+    square.right(90)     
+turtle.done()
